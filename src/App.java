d69e80420dd6d50af24ea823f3c034ad580fe8cd
Carlos <carlosicaza990@gmail.com>
2025-08-05 13:00:36 -0400

    Add new functions

--- src/App.java
+++ src/App.java
@@ -2,6 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("OAAAAAAAAAAAAAAA");
-        System.out.println("aaa");
+        System.out.println("prueba");
+        int i = 1;
+        i = i + 1;
+        System.out.println("Incremented value: " + i);
+        System.out.println("End of program.");
     }
 }
